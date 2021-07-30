ffd9885a7c352156817acd2b4cae508edcbb5f37
Данил Картамышев <danil.kartamyshev@gmail.com>
2021-07-30 16:08:16 +0300

    Add new exercize

---
+++ src/LinkedList/TwoPointerTechnique/IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,70 @@
+﻿namespace LinkedList.TwoPointerTechnique
+{
+    public class IntersectionOfTwoLinkedLists
+    {
+        public class Solution
+        {
+            public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+            {
+                ListNode currentA = headA;
+                ListNode currentB = headB;
+
+                int countA = 0;
+                int countB = 0;
+
+                while (currentA != null || currentB != null)
+                {
+                    if (currentA == currentB)
+                        return currentA;
+
+                    if (currentA != null)
+                    {
+                        countA++;
+                        currentA = currentA.next;
+                    }
+
+                    if (currentB != null)
+                    {
+                        countB++;
+                        currentB = currentB.next;
+                    }
+                }
+
+                if (countA == countB)
+                    return null;
+
+                currentA = Shift(headA, countA - countB);
+                currentB = Shift(headB, countB - countA);
+
+                ListNode intersection = null;
+
+                while (intersection == null && currentA != null && currentB != null)
+                {
+                    if (currentA == currentB)
+                    {
+                        intersection = currentA;
+                    }
+
+                    currentA = currentA.next;
+                    currentB = currentB.next;
+                }
+
+                return intersection;
+            }
+
+            private ListNode Shift(ListNode head, int count)
+            {
+                if (count <= 0)
+                    return head;
+
+                while (count > 0)
+                {
+                    head = head.next;
+                    count--;
+                }
+
+                return head;
+            }
+        }
+    }
+}
---
+++ src/LinkedListTests/TwoPointerTechnique/IntersectionOfTwoLinkedListsTests.cs
@@ -0,0 +1,47 @@
+﻿using LinkedList;
+using LinkedList.TwoPointerTechnique;
+using Xunit;
+
+namespace LinkedListTests.TwoPointerTechnique
+{
+    public class IntersectionOfTwoLinkedListsTests
+    {
+        private IntersectionOfTwoLinkedLists.Solution _solution;
+
+        public IntersectionOfTwoLinkedListsTests()
+        {
+            _solution = new IntersectionOfTwoLinkedLists.Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 1 }, new int[] { 5, 6, 1, }, new int[] { 8, 4, 5 })]
+        [InlineData(new int[] { 4, 1, 7 }, new int[] { 5, 6, }, new int[] { 8, 4, 2 })]
+        [InlineData(new int[] { 4, 1, 7 }, new int[] { 5, 6, 1 }, new int[] { 8, 4, 2 })]
+        [InlineData(new int[] { 1, 9, 1 }, new int[] { 3 }, new int[] { 2, 4 })]
+        [InlineData(new int[] { 1 }, new int[] { 2 }, new int[] { 3 })]
+        public void GetIntersectionNode_return_node(int[] valuesA, int[] valuesB, int[] valuesTail)
+        {
+            (ListNode headA, ListNode headB, ListNode intersection) = Helper.CreateIntersection(valuesA, valuesB, valuesTail);
+
+            ListNode actual = _solution.GetIntersectionNode(headA, headB);
+
+            Assert.Equal(intersection, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 1 }, new int[] { 5, 6, 1, })]
+        [InlineData(new int[] { 4, 1, 7 }, new int[] { 5, 6, })]
+        [InlineData(new int[] { 4, 1, 7 }, new int[] { 5, 6, 1 })]
+        [InlineData(new int[] { 1, 9, 1 }, new int[] { 3 })]
+        [InlineData(new int[] { 2, 6, 4 }, new int[] { 1, 5 })]
+        public void GetIntersectionNode_return_null(int[] valuesA, int[] valuesB)
+        {
+            (ListNode headA, _) = Helper.CreateList(valuesA);
+            (ListNode headB, _) = Helper.CreateList(valuesB);
+            
+            ListNode actual = _solution.GetIntersectionNode(headA, headB);
+
+            Assert.Null(actual);
+        }
+    }
+}
