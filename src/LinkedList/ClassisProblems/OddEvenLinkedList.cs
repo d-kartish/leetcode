419034708f89ef2adec7f70cffbe91c787106dd1
Данил Картамышев <danil.kartamyshev@gmail.com>
2021-08-03 12:57:36 +0300

    Add exercise Odd even linked list

---
+++ src/LinkedList/ClassisProblems/OddEvenLinkedList.cs
@@ -0,0 +1,38 @@
+﻿namespace LinkedList.ClassisProblems
+{
+    public class OddEvenLinkedList
+    {
+        public class Solution
+        {
+            public ListNode OddEvenList(ListNode head)
+            {
+                if (head == null || head.next == null)
+                    return head;
+
+                ListNode odd = head;
+                ListNode even = head.next;
+                ListNode evenHead = even;
+
+                while (odd.next != null && even.next != null)
+                {
+                    if (odd.next.next != null)
+                    {
+                        odd.next = odd.next.next;
+                        odd = odd.next;
+                    }
+
+                    if (even.next.next != null)
+                    {
+                        even.next = even.next.next;
+                        even = even.next;
+                    }
+                }
+
+                odd.next = evenHead;
+                even.next = null;
+
+                return head;
+            }
+        }
+    }
+}
---
+++ src/LinkedListTests/ClassisProblems/OddEvenLinkedListTests.cs
@@ -0,0 +1,37 @@
+﻿using LinkedList;
+using LinkedList.ClassisProblems;
+using Xunit;
+
+namespace LinkedListTests.ClassisProblems
+{
+    public class OddEvenLinkedListTests
+    {
+        private OddEvenLinkedList.Solution _solution;
+
+        public OddEvenLinkedListTests()
+        {
+            _solution = new OddEvenLinkedList.Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 3, 5, 2, 4 })]
+        [InlineData(new int[] { 2, 1, 3, 5, 6, 4, 7 }, new int[] { 2, 3, 6, 7, 1, 5, 4 })]
+        [InlineData(new int[] { 1 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void OddEvenList(int[] values, int[] expected)
+        {
+            (ListNode head, _) = Helper.CreateList(values);
+
+            ListNode actual = _solution.OddEvenList(head);
+
+            ListNode current = actual;
+
+            foreach (int value in expected)
+            {
+                Assert.Equal(value, current.val);
+                current = current.next;
+            }
+        }
+    }
+}
