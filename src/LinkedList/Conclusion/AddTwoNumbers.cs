b9685498d9dc7b2218c22c1f95a05abb457d3ea3
Данил Картамышев <danil.kartamyshev@gmail.com>
2021-08-04 16:07:50 +0300

    Add exercise Add Two Numbers

---
+++ src/LinkedList/Conclusion/AddTwoNumbers.cs
@@ -0,0 +1,44 @@
+﻿namespace LinkedList.Conclusion
+{
+    public class AddTwoNumbers
+    {
+        public class Solution
+        {
+            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+                ListNode currentA = l1;
+                ListNode currentB = l2;
+
+                ListNode head = new ListNode();
+                ListNode current = head;
+
+                while (currentA != null || currentB != null)
+                {
+                    AddValueAndShift(ref currentA, ref current.val);
+                    AddValueAndShift(ref currentB, ref current.val);
+
+                    int value = current.val / 10;
+
+                    if (value > 0 || currentA != null || currentB != null)
+                    {
+                        current.next = new ListNode(value);
+                    }
+
+                    current.val %= 10;
+                    current = current.next;
+                }
+
+                return head;
+            }
+
+            private void AddValueAndShift(ref ListNode current, ref int sum)
+            {
+                if (current == null)
+                    return;
+
+                sum += current.val;
+                current = current.next;
+            }
+        }
+    }
+}
---
+++ src/LinkedListTests/Conclusion/AddTwoNumbersTests.cs
@@ -0,0 +1,41 @@
+﻿using LinkedList;
+using LinkedList.Conclusion;
+using Xunit;
+
+namespace LinkedListTests.Conclusion
+{
+    public class AddTwoNumbersTests
+    {
+        private AddTwoNumbers.Solution _solution;
+
+        public AddTwoNumbersTests()
+        {
+            _solution = new AddTwoNumbers.Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8 })]
+        [InlineData(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4, 9, 7 }, new int[] { 7, 0, 8, 9, 7 })]
+        [InlineData(new int[] { 2, 4, 3, 9, 7 }, new int[] { 5, 6, 4 }, new int[] { 7, 0, 8, 9, 7 })]
+        [InlineData(new int[] { 0 }, new int[] { 0 }, new int[] { 0 })]
+        [InlineData(new int[] { 9, 9, 9, 9, 9, 9, 9 }, new int[] { 9, 9, 9, 9 }, new int[] { 8, 9, 9, 9, 0, 0, 0, 1 })]
+        public void AddTwoNumbers(int[] valuesA, int[] valuesB, int[] expected)
+        {
+            (ListNode headA, _) = Helper.CreateList(valuesA);
+            (ListNode headB, _) = Helper.CreateList(valuesB);
+
+            ListNode actual = _solution.AddTwoNumbers(headA, headB);
+
+            ListNode current = actual;
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], current.val);
+
+                current = current.next;
+            }
+
+            Assert.Null(current);
+        }
+    }
+}
