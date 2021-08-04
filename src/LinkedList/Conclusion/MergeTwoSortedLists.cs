c69fc0b5d6fd83a78a51314c7968b4eeffd01897
Данил Картамышев <danil.kartamyshev@gmail.com>
2021-08-04 12:15:58 +0300

    Add exercise Merge Two Sorted Lists

---
+++ src/LinkedList/Conclusion/MergeTwoSortedLists.cs
@@ -0,0 +1,55 @@
+﻿namespace LinkedList.Conclusion
+{
+    public class MergeTwoSortedLists
+    {
+        public class Solution
+        {
+            private static ListNode empty = new ListNode();
+
+            public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+            {
+                if (l1 == null || l2 == null)
+                    return l1 ?? l2;
+
+                ListNode currentA = l1;
+                ListNode currentB = l2;
+
+                ListNode head = empty;
+                ListNode current = head;
+
+                while (currentA != null || currentB != null)
+                {
+                    ListNode prev;
+
+                    if (currentA == null)
+                    {
+                        Shift(ref currentB, out prev);
+                    }
+                    else if (currentB == null)
+                    {
+                        Shift(ref currentA, out prev);
+                    }
+                    else if (currentA.val > currentB.val)
+                    {
+                        Shift(ref currentB, out prev);
+                    }
+                    else
+                    {
+                        Shift(ref currentA, out prev);
+                    }
+
+                    current.next = prev;
+                    current = current.next;
+                }
+
+                return head.next;
+            }
+
+            private void Shift(ref ListNode current, out ListNode prev)
+            {
+                prev = current;
+                current = current.next;
+            }
+        }
+    }
+}
---
+++ src/LinkedListTests/Conclusion/MergeTwoSortedListsTests.cs
@@ -0,0 +1,48 @@
+﻿using LinkedList;
+using LinkedList.Conclusion;
+using Xunit;
+
+namespace LinkedListTests.Conclusion
+{
+    public class MergeTwoSortedListsTests
+    {
+        private MergeTwoSortedLists.Solution _solution;
+
+        public MergeTwoSortedListsTests()
+        {
+            _solution = new MergeTwoSortedLists.Solution();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
+        [InlineData(new int[] { 4, 5, 6 }, new int[] { 1, 2, 3 }, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 }, new int[] { 1, 1, 1, 1, 1, 1 })]
+        [InlineData(new int[] { 1, 2, 4 }, new int[] { }, new int[] { 1, 2, 4 })]
+        [InlineData(new int[] { }, new int[] { 1, 3, 4 }, new int[] { 1, 3, 4 })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { })]
+        public void MergeTwoLists(int[] valuesA, int[] valuesB, int[] expected)
+        {
+            (ListNode headA, _) = Helper.CreateList(valuesA);
+            (ListNode headB, _) = Helper.CreateList(valuesB);
+
+            ListNode actual = _solution.MergeTwoLists(headA, headB);
+
+            if (expected.Length == 0)
+            {
+                Assert.Null(actual);
+            }
+            else
+            {
+                ListNode current = actual;
+
+                for (int i = 0; i < expected.Length; i++)
+                {
+                    Assert.Equal(expected[i], current.val);
+
+                    current = current.next;
+                }
+            }
+        }
+    }
+}
